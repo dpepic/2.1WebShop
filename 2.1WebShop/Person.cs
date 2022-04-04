298b035971117dcad4ddd7bde2715e52299bdf52
sead_ <sead_@LENOVO>
2022-04-04 19:13:30 +0200

    Add project files.

---
+++ 2.1WebShop/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._1WebShop
+{
+    internal abstract class Person
+    {
+        public string _name = string.Empty;
+        public string _surname = string.Empty;
+        public string _email = string.Empty;
+        public string _username = string.Empty;
+        public string _password = string.Empty;
+
+        public Address _address = new();
+    }
+}
